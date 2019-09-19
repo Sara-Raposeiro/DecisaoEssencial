8bf78fc6192e98dd745ffcc1f234c8eff5aaa809
Sara-Raposeiro <S.SequeiraRaposeiro@student.tudelft.nl>
2019-09-19 19:05:54 +0200

    Create carreiras.js

---
+++ carreiras.js
@@ -0,0 +1,65 @@
+
+// Get the 17th  modal
+var modal17 = document.getElementById("myModal17");
+// Get the button that opens the modal
+var btn17 = document.getElementById("myBtn17");
+// Get the <span> element that closes the modal
+var span17 = document.getElementsByClassName("close17")[0];
+// When the user clicks on the button, open the modal
+btn17.onclick = function() {
+  modal17.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span17.onclick = function() {
+  modal17.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal17) {
+    modal17.style.display = "none";
+  }
+}
+
+
+// Get the 18th  modal
+var modal18 = document.getElementById("myModal18");
+// Get the button that opens the modal
+var btn18 = document.getElementById("myBtn18");
+// Get the <span> element that closes the modal
+var span18 = document.getElementsByClassName("close18")[0];
+// When the user clicks on the button, open the modal
+btn18.onclick = function() {
+  modal18.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span18.onclick = function() {
+  modal18.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal18) {
+    modal18.style.display = "none";
+  }
+}
+
+
+// Get the 19th  modal
+var modal19 = document.getElementById("myModal19");
+// Get the button that opens the modal
+var btn19 = document.getElementById("myBtn19");
+// Get the <span> element that closes the modal
+var span19 = document.getElementsByClassName("close19")[0];
+// When the user clicks on the button, open the modal
+btn19.onclick = function() {
+  modal19.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span19.onclick = function() {
+  modal19.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal19) {
+    modal19.style.display = "none";
+  }
+}
